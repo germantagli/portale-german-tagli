a36a1caa57f1f6954e89ab678b9e9f2fb7e26eed
GERMAN\germa <germantagli@gmail.com>
2023-08-24 15:11:26 +0200

    fix footer component

--- src/components/FooterComponent/FooterComponent.js
+++ src/components/FooterComponent/FooterComponent.js
@@ -1,23 +1,30 @@
+import { Container, Row, Nav, Navbar, Col } from "react-bootstrap";
 import "./FooterComponent.scss";
 import PropTypes from "prop-types";
+import { useTranslation } from "react-i18next";
 
 /**
- * @function FooterComponent test components
- * @param info information that arrives procedurally provided
+ * @function FooterComponent Footer components
  * @returns {JSX.Element}
 */
-const FooterComponent = ({ info }) => { 
+const FooterComponent = ({ info }) => {
+    const [tranlastion] = useTranslation("global")
     return (
-       <div>Footer</div> 
+        <div className="FooterComponent">
+            <div className="justify-content-center text-center wrapLinks">
+                <a href="#about">{tranlastion("header.about")}</a>
+                <a href="#skills">{tranlastion("header.skills")}</a>
+                <a href="#experience">{tranlastion("header.experience")}</a>
+                <a href="#portfolio">{tranlastion("header.portfolio")}</a>
+                <a href="#contact">{tranlastion("header.contact")}</a>
+            </div>
+
+        </div>
     )
 };
 
-FooterComponent.propTypes = {
-    info: PropTypes.string.isRequired,
-};
+FooterComponent.propTypes = {};
 
-FooterComponent.defaultProps = {
-    info: "",
-};
+FooterComponent.defaultProps = {};
 
 export default FooterComponent;
